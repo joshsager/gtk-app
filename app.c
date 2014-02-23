be084c172ba191e02438aafe0afe176d421d7efb
joshsager <sager379@gmail.com>
2014-02-23 16:36:50 -0500

    Creates label and updates label on button click

--- app.c
+++ app.c
@@ -4,13 +4,27 @@
 	@description: playground application for me to learn how to write software using gtk
 */
 #include <gtk/gtk.h>
-// TODO: Create a label
-// TOOD: Update the label when button is pressed
 // TODO: Insert an image
 
-GtkWidget *button; 
-GtkWidget *window;
+gint count = 0;
+char buf[5];
 
+void increase(GtkWidget *widget, gpointer label)
+{
+	count++;
+
+	sprintf(buf, "%d", count);
+	gtk_label_set_text(GTK_LABEL(label), buf);
+}
+
+void decrease(GtkWidget *widget, gpointer label)
+{
+	count--;
+
+	sprintf(buf, "%d", count);
+	gtk_label_set_text(GTK_LABEL(label), buf);
+
+}
 /*
 	Loads in the icon
 */
@@ -31,19 +45,42 @@ GdkPixbuf *create_pixbuf(const gchar * filename)
 */
 void drawWindow()
 {
+
+	GtkWidget *window;
+	GtkWidget *label;
+	GtkWidget *frame;
+	GtkWidget *plus;
+	GtkWidget *minus;
+
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(GTK_WINDOW(window), "icon");
+	gtk_window_set_title(GTK_WINDOW(window), "Plus or Minus");
 	gtk_window_set_default_size(GTK_WINDOW(window), 400,200);
 	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
 	gtk_window_set_icon(GTK_WINDOW(window), create_pixbuf("icon.png"));
 
-	gtk_container_set_border_width (GTK_CONTAINER (window), 10);
-	button = gtk_button_new_with_label ("hello world");
-	gtk_container_add (GTK_CONTAINER (window), button);
+	frame = gtk_fixed_new();
+	gtk_container_add(GTK_CONTAINER(window), frame);
 
-	gtk_widget_show (button);
-	gtk_widget_show (window);
+	plus = gtk_button_new_with_label("+");
+	gtk_widget_set_size_request(plus, 80, 35);
+	gtk_fixed_put(GTK_FIXED(frame), plus, 50, 20);
 
+	minus = gtk_button_new_with_label("-");
+	gtk_widget_set_size_request(minus, 80, 35);
+	gtk_fixed_put(GTK_FIXED(frame), minus, 50, 80);
+
+	label = gtk_label_new("0");
+	gtk_fixed_put(GTK_FIXED(frame), label, 190, 58);
+
+	// gtk_container_set_border_width (GTK_CONTAINER (window), 10);
+	// button = gtk_button_new_with_label ("hello world");
+	// gtk_container_add (GTK_CONTAINER (window), button);
+
+	// gtk_widget_show (button);
+	gtk_widget_show_all(window);
+	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	g_signal_connect(plus, "clicked", G_CALLBACK(increase), label);
+	g_signal_connect(minus, "clicked", G_CALLBACK(decrease), label);
 	gtk_main();
 }
 /*static void hello(GtkWidget *widget, gpointer data){
